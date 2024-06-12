d4948eccddaf91da5221cbd7c47773e738e09fbe
alice.boyer <alice.boyer@u-paris.fr>
2024-06-12 21:02:17 +0200

    add python higlight

--- init.lua
+++ init.lua
@@ -820,7 +820,7 @@ require('lazy').setup({
     },
     build = ':TSUpdate',
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc' },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'python', 'vim', 'vimdoc' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
